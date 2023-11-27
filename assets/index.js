ffc74d62ce4d962e31b4bae655e743fc216db06d
Nhu-Nguyen <121688890+nhunguyen-debug@users.noreply.github.com>
2023-11-26 22:28:03 -0600

    adding play again button with js

--- assets/index.js
+++ assets/index.js
@@ -66,4 +66,48 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         return array;
     }
+    // Add event listener for the "Play Again" button
+    const playAgainButton = document.getElementById('play-again-btn');
+    playAgainButton.addEventListener('click', playAgain);
+
+    // Function to reset the game
+    function playAgain() {
+        // Reset the game state
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            card.style.display = 'block';
+            card.classList.remove('flipped');
+        });
+
+        // Reshuffle the cards
+        const shuffledSymbols = shuffleArray(symbols.concat(symbols));
+        for (let i = 0; i < totalCards; i++) {
+            const card = cards[i];
+            const symbol = card.querySelector('.symbol');
+            symbol.textContent = shuffledSymbols[i];
+        }
+
+        // Hide the "Play Again" button
+        playAgainButton.style.display = 'none';
+    }
+
+    // Function to check for a win
+    function checkWin() {
+        const remainingCards = document.querySelectorAll('.card:not([style="display: none;"])');
+        if (remainingCards.length === 0) {
+            // All cards are matched, display "You win"
+            alert('You win!');
+            // Show the "Play Again" button
+            playAgainButton.style.display = 'block';
+        }
+    }
+
+    // Your existing shuffleArray function
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
 });
